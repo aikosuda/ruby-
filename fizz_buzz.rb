581f7393c5be1c437a6497d98ecc7d7a852a5e15
須田藍子 <aiko05.hello@gmail.com>
2020-07-02 14:42:29 +0900

    [finish]3-9

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,19 @@
-def hello_world
-	'Hello, World!'
+def fizz_bazz(number)
+	if number % 15 == 0
+		 'FizzBuzz'
+	elsif number % 3 == 0
+		 'Fizz'
+	elsif number % 5 == 0
+		 'Buzz'
+	else  
+		number.to_s
+	end
 end
+
+puts "1　以上の数字を入力してください"
+
+input = gets.to_i
+
+puts '結果は、、、'
+
+puts fizz_bazz(input)
